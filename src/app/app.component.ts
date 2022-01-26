9fc4544eaf4f948956da4a5fff129f6e94ece236
Francesco Giancipoli <francescogiancipoli15@gmail.com>
2022-01-26 20:43:01 +0100

    Fix errors

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { WeatherService } from './weather.service';
+import { HttpClient } from '@angular/common/http';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnDestroy {
   title = 'Weather';
+  weather: any;
+  mySubscription: Subscription
+  myTemp: any;
+
+  constructor(
+    private weatherService: WeatherService,
+    private httpClient: HttpClient
+    ){}
+
+  uploadFile(file){
+    this.httpClient.get('assets/weather.dat', { responseType: 'text' }).subscribe(
+      data => {
+        this.weather = JSON.stringify(data);
+        this.myTemp = this.weatherService.takeTemp(this.weather);
+      }
+    );
+
+    
+  }
+
+  ngOnDestroy(): void {
+    this.mySubscription.unsubscribe();
+  }
+
+
+
 }
---
+++ src/app/weather.service.ts
@@ -0,0 +1,81 @@
+import { Injectable } from '@angular/core';
+
+interface Temp {
+    day: number;
+    diff: number;
+}
+
+@Injectable({
+    providedIn: 'root'
+})
+
+export class WeatherService {
+
+    colderT: number;
+
+    // Estrae l'array con le temperature piu alte
+    takeTemp(weather: any){
+        let hotTemp = 0;
+        let coldTemp = 0;
+        let day = 0;
+        let colIndex = 0;
+        let temp: Temp[] = [];
+        let rowIndex = 1;
+        weather.split(" ").map(x => {
+            if((x.match(/\d/g) && !x.match(/1HrP/g)) || x.match(/mo/g)){
+                colIndex++;
+            }
+            if(colIndex == 1 && x != ""){
+                day = +(x.match(/\d+/));
+            }
+            if(colIndex == 2 && x != ""){
+                if(x.match(/..(?=(\*))/g)){
+                    hotTemp = +(x.match(/..(?=(\*))/g));
+                } else{
+                    hotTemp = +(x);
+                }
+            }
+            if(colIndex == 3 && x != ""){
+                if(x.match(/..(?=(\*))/g)){
+                    coldTemp = +(x.match(/..(?=(\*))/g));
+                } else{
+                    coldTemp = +(x);
+                }
+            }
+            if(hotTemp && coldTemp) {
+                let diff = this.getDifference(hotTemp, coldTemp);
+                let objTemp = {day, diff};
+                temp.push(objTemp);
+                hotTemp = 0;
+                coldTemp = 0;
+            }
+            if(x.match(/\d{4}/g)){
+                rowIndex++;
+                colIndex = 0;
+            }
+        })
+        let myTemp = this.minTDay(temp);
+        return myTemp;
+    }
+
+    getDifference(hotTemp: number, coldTemp: number){
+        let result = hotTemp - coldTemp;
+        return result;
+    }
+
+    // Estrae l'escursione termica piu piccola
+    minTDay(temp: Temp[]){
+        let i = 0
+        this.colderT = temp[0].diff;
+        temp.forEach((t)=>{
+            if(t.diff < this.colderT){
+                this.colderT = t.diff;
+                i = t.day;
+            }
+        })
+        return {day: i, diff: this.colderT};
+    }
+
+
+
+}
